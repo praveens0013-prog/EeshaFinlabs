2765afeb5b8c7898dab311b31a523dcb31907dc9
praveens0013-prog <praveen.s0013@gmail.com>
2025-12-07 21:11:07 +0530

    Update Apply.jsx

--- src/components/Apply.jsx
+++ src/components/Apply.jsx
@@ -2,13 +2,12 @@ export default function Apply() {
   return (
     <div style={{ width: "100%", minHeight: "100vh" }}>
       <iframe
-        src="<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfX3bHurKbE2PVYV4NXEvodQFiZXsuN-JZCj-RL3__7EID13A/viewform?embedded=true" width="100" height="900" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>"
+        src="https://docs.google.com/forms/d/e/EeshaFinlabs_Leads (Responses)/viewform?embedded=true"
         width="100%"
         height="900"
-        frameBorder="0"
-      >
-        Loading…
-      </iframe>
+        frameBorder={0}
+        title="Eesha Finlabs Lead Form"
+      />
     </div>
   );
 }
