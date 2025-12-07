60c8f92c6268a61b0465d7ba45d6fdaa09a34ab9
praveens0013-prog <praveen.s0013@gmail.com>
2025-12-07 16:56:09 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",   // ✅ ADD THIS LINE
+  base: "/",
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
-
