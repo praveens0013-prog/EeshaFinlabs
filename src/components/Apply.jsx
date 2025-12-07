c782cf151dbe03e6b33ab63b4c4be6c2f6e39897
praveens0013-prog <praveen.s0013@gmail.com>
2025-12-07 20:55:26 +0530

    Create Apply.jsx

---
+++ src/components/Apply.jsx
@@ -0,0 +1,14 @@
+export default function Apply() {
+  return (
+    <div style={{ width: "100%", minHeight: "100vh" }}>
+      <iframe
+        src="<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfX3bHurKbE2PVYV4NXEvodQFiZXsuN-JZCj-RL3__7EID13A/viewform?embedded=true" width="100" height="900" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>"
+        width="100%"
+        height="900"
+        frameBorder="0"
+      >
+        Loading…
+      </iframe>
+    </div>
+  );
+}
